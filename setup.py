a1ac1344595db3a379b3b72339019d1a7a8158f0
Theo Dedeken <theo.dedeken@telenet.be>
2019-02-22 17:59:10 +0100

    Init package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dimdrop-theozonebe",
+    version="0.0.1",
+    author="Theo Dedeken",
+    author_email="theo.dedeken@telenet.be",
+    description="Providing some dimensionality reduction methods using Machine Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheOZoneBE/dimdrop",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
