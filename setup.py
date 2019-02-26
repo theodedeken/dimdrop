051a7a49ce94cf86abc088e5b11c58b07f8c889e
Theo Dedeken <theo.dedeken@telenet.be>
2019-02-26 16:05:25 +0100

    change name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dimdrop-theozonebe",
+    name="dimdrop",
     version="0.0.1",
     author="Theo Dedeken",
     author_email="theo.dedeken@telenet.be",
