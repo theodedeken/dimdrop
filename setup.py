e23060f785e281acac84d6743ecc7207d88c5530
Theo Dedeken <theo.dedeken@telenet.be>
2019-08-06 11:11:36 +0200

    fix url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Providing some dimensionality reduction methods using Machine Learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TheOZoneBE/dimdrop",
+    url="https://github.com/theodedeken/dimdrop",
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy",
